ffc52be3628b4ef19795244d421aa9dad1e5edd6
Joshua Yalung <112442856+jcyalung@users.noreply.github.com>
2024-04-10 20:24:46 -0700

    update sqlitehelpesr

--- sqlite_helpers.py
+++ sqlite_helpers.py
@@ -2,25 +2,34 @@
 This file contains helper functions for creating and managing sqlite databases.
 Created by Joshua Yalung.
 """
+import sqlite3
+connection = sqlite3.connect('urls.db') # connect to database (creates if none found)
+cursor = connection.cursor()
 
 # this function creates a table in the database
 def table_creation():
     # the function creates a table that has:
     # url, alias, timestamp, and id.
-    print("create table")
+    cursor.execute("CREATE TABLE url(link, alias, timestamp, id)")
 
-def table_insert():
+def table_insert(link, alias, timestamp, id):
     # the function inserts a row into the table
-    print("insert into table")
+    input = f'INSERT INTO urls VALUES({link}, {alias}, {timestamp}, {id})'
+    cursor.execute(input)
+    connection.commit()
 
-def table_delete():
+def table_delete(link):
     # the function deletes a row from the table
-    print("delete from table")
+    input = f'DELETE FROM urls WHERE link={link}'
+    cursor.execute(input)
+    connection.commit()
 
 def table_list():
     # the function lists all the rows in the table
-    print("list table")
+    result = cursor.execute("SELECT * FROM urls")
+    return result.fetchall()
 
-def table_retrieve():
+def table_retrieve(alias):
     # the function retrieves a row from the table
-    print("retrieve from table")
+    result = cursor.execute(f'SELECT link, alias, timestamp, id FROM urls WHERE alias={alias}')
+    return result.fetchone()
